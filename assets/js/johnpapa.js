ba67d3395126f430dc9ec1b1932da9334d07d3fb
John Papa <johnpapa@users.noreply.github.com>
2016-05-07 14:47:16 -0400

    Update and rename spboyer.js to johnpapa.js

--- assets/js/johnpapa.js
+++ assets/js/johnpapa.js
@@ -1,16 +1,16 @@
 // Developer media ads
 function _dmBootstrap(file) {
-  var _dma = document.createElement('script');
-  _dma.type = 'text/javascript';
-  _dma.async = true;
-  _dma.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + file;
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(_dma);
+    var _dma = document.createElement('script');
+    _dma.type = 'text/javascript';
+    _dma.async = true;
+    _dma.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + file;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(_dma);
 }
 function _dmFollowup(file) { if (typeof DMAds === 'undefined') _dmBootstrap('cdn2.DeveloperMedia.com/a.min.js'); }
 (function () { _dmBootstrap('cdn1.DeveloperMedia.com/a.min.js'); setTimeout(_dmFollowup, 2000); })();
 
 // Disqus
-var disqus_shortname = 'spboyer';
+var disqus_shortname = 'johnpapa7';
 (function () {
     var s = document.createElement('script');
     s.async = true;
@@ -25,5 +25,5 @@ var disqus_shortname = 'spboyer';
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 // set id
-ga('create', 'UA-48228610-1', 'auto');
+ga('create', 'UA-18897867-1', 'auto');
 ga('send', 'pageview');
